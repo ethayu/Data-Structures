517210782894e62968b82bf1756a867310d49586
ethayu <ethayu1@gmail.com>
2021-09-16 00:22:10 -0700

    update

--- src/InheritA1v2.java
+++ src/InheritA1v2.java
@@ -2,7 +2,7 @@ import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,15 +13,21 @@ public class InheritA1v2 extends GraphicsProgram {
         new InheritA1v2().start(args);
     }
 
+    /**
+     * runs field
+     */
     public void run() {
         GRect rectangleRange = new GRect(arenaSize, arenaSize);
         add(rectangleRange);
         System.out.print("How many bawls?: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        System.out.print("Detect Collisions (Y/n): ");
+        boolean detect = sc.next().charAt(0) == 'Y';
         ArrayList<Ball> balls = new ArrayList<>(n);
+        Color[] colors = {Color.BLACK, Color.BLUE, Color.CYAN, Color.GRAY, Color.GREEN, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.RED, Color.WHITE, Color.YELLOW};
         for (int i = 0; i < n; i++) {
-            balls.add(new Ball((i + 1) * arenaSize / (n + 1), (i + 1) * arenaSize / (n + 1), 10, 10, Color.BLUE));
+            balls.add(new Ball((i + 1) * arenaSize / (n + 1), (i + 1) * arenaSize / (n + 1), 10, 10, colors[i % 11]));
             add(balls.get(i));
         }
         while (true) {
@@ -33,7 +39,7 @@ public class InheritA1v2 extends GraphicsProgram {
                 for (int j = i + 1; j < balls.size(); j++) {
                     Ball otherball = balls.get(j);
                     if (!otherball.isVisible()) continue;
-                    if (Ball.isColliding(ball, otherball)) {
+                    if (Ball.isColliding(ball, otherball) && detect) {
                         otherball.setVisible(false);
                         ball.theta = (int) (Math.random() * 360);
                     }
@@ -44,11 +50,16 @@ public class InheritA1v2 extends GraphicsProgram {
         }
     }
 
+    /**
+     * Ball class. Keeps track of current location in cartesian coordinates as well as angle (theta)
+     */
     static class Ball extends GOval {
         public static final int radius = 5;
-        int theta = (int) (Math.random() * 360), x = Math.toIntExact(Math.round(getX())), y = Math.toIntExact(Math.round(getY())), r = 1;
+        int theta = (int) (Math.random() * 360), x = Math.toIntExact(Math.round(getX())), y = Math.toIntExact(Math.round(getY()));
 
-
+        /**
+         * action done when ball hits one of the vertical sides.
+         */
         public void xB() {
             theta = 180 - theta;
             theta %= 360;
@@ -57,6 +68,9 @@ public class InheritA1v2 extends GraphicsProgram {
             else x = arenaSize - 1;
         }
 
+        /**
+         * action done when ball hits one of the horizontal sides.
+         */
         public void yB() {
             theta *= -1;
             theta %= 360;
@@ -65,18 +79,36 @@ public class InheritA1v2 extends GraphicsProgram {
             else y = arenaSize - 1;
         }
 
+        /**
+         * Ball constructor that creates GOval and sets location, size, color based on input and sets Fill to true.
+         * @param a
+         * @param b
+         * @param v
+         * @param v1
+         * @param c
+         */
         public Ball(double a, double b, double v, double v1, Color c) {
             super(a, b, v, v1);
             setFilled(true);
             setColor(c);
         }
 
+        /**
+         * Moves ball by inputted unit in current direction (theta). Also updates current position.
+         * @param a
+         */
         public void move(double a) {
             movePolar(a, theta);
             x = Math.toIntExact(Math.round(getX()));
             y = Math.toIntExact(Math.round(getY()));
         }
 
+        /**
+         * returns true if Balls a and b collide.
+         * @param a
+         * @param b
+         * @return
+         */
         static boolean isColliding(Ball a, Ball b) {
             return (a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()) <= 4 * radius * radius;
         }
