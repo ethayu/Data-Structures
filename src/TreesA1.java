ec99abf038c0045530531cf52d23bb57337fb716
Ethan Yu <ethayu1@gmail.com>
2022-03-06 01:45:12 -0800

    update

---
+++ src/TreesA1.java
@@ -0,0 +1,55 @@
+public class TreesA1 {
+
+    class BSTree<E extends Comparable<E>>{
+        private TreeNode root;
+
+        public BSTree(){
+            root = null;
+        }
+
+
+    }
+
+}
+
+class TreeNode {
+    private Object value;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(Object initValue) {
+        value = initValue;
+        left = null;
+        right = null;
+    }
+
+    public TreeNode(Object initValue, TreeNode initLeft, TreeNode initRight) {
+        value = initValue;
+        left = initLeft;
+        right = initRight;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setValue(Object theNewValue) {
+        value = theNewValue;
+    }
+
+    public void setLeft(TreeNode theNewLeft) {
+        left = theNewLeft;
+    }
+
+    public void setRight(TreeNode theNewRight) {
+        right = theNewRight;
+    }
+}
